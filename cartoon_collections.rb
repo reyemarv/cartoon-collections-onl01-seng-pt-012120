7fbb899bd34db42b770a57802b081d5c8bbc1c9a
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 17:23:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,10 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(words)
-  words.any? {|w| w.length > 4}
+  words.any?{|w| w.length > 4}
 end
 
 def find_the_cheese#(food)
-  food.find{
+  food.find{|c| c.is_a?(chese_types)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
