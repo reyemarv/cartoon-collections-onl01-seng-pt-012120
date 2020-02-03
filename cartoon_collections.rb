1b35cd587581423495cb434cf409ba8c322ef394
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 16:39:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(group)
-  group.each.with_index 
+  group.each.with_index(i) |dwarf, i|
+    puts "#{i} #{dwarf}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
